573a39a334f25cff0fad56c86ecf8740a8c58964
bruno <seta@192.168.1.34>
2023-10-26 14:51:41 -0300

    blogs page and pagination snippet

--- assets/application.js
+++ assets/application.js
@@ -1,8 +1,11 @@
 // Put your application javascript here
 // Sort products inside collection
-document.querySelector('#sort_by').addEventListener('change', (e) => {
-  const url = new URL(window.location.href);
 
-  url.searchParams.set('sort_by', e.currentTarget.value);
-  window.location = url.href;
-})
+if (document.getElementById('sort_by')) {
+  document.querySelector('#sort_by').addEventListener('change', (e) => {
+    const url = new URL(window.location.href);
+
+    url.searchParams.set('sort_by', e.currentTarget.value);
+    window.location = url.href;
+  });
+}
