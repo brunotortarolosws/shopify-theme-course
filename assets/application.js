c12717f5e45550cba8223c7f7a2ca970ae0faefc
bruno <seta@brunos-MacBook-Air.local>
2023-11-02 16:49:22 -0300

    created form-error snippet, created account, addresses, login, register templates, added js script to handle province select's options

--- assets/application.js
+++ assets/application.js
@@ -9,3 +9,22 @@ if (document.getElementById('sort_by')) {
     window.location = url.href;
   });
 }
+
+if (document.getElementById('AddressCountryNew')) {
+  document.getElementById('AddressCountryNew').addEventListener('change', function(e) {
+    const provinces = this.options[this.selectedIndex].getAttribute('data-provinces');
+    const provinceSelector = document.getElementById('AddressProvinceNew');
+    const provinceArray = JSON.parse(provinces);
+    let provinceOptions = '';
+
+    !provinceArray.length
+      ? provinceSelector.setAttribute('disabled', 'disabled')
+      : provinceSelector.removeAttribute('disabled');
+
+    provinceArray.forEach(p => {
+      provinceOptions += `<option value="${p[0]}">${p[0]}</option>`;
+    });
+
+    provinceSelector.innerHTML = provinceOptions;
+  });
+}
